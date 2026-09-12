16e50570e48e32882a0a12270e4901578cf9731b
Mordread <patrick.winn@gmail.com>
2026-09-12 17:04:43 -0500

    chore: log MainRig remote launcher

--- scripts/mainrig/start-remote.cmd
+++ scripts/mainrig/start-remote.cmd
@@ -1,10 +1,12 @@
 @echo off
 setlocal
 set "REPO=C:\Dev\AI\DesktopCommanderMCP"
-set "HOME=%LOCALAPPDATA%\DesktopCommanderMCP-MainRig\home"
+set "STATE=%LOCALAPPDATA%\DesktopCommanderMCP-MainRig"
+set "LOG=%STATE%\remote.log"
 
-if not exist "%HOME%" mkdir "%HOME%"
+if not exist "%STATE%" mkdir "%STATE%"
 cd /d "%REPO%" || exit /b 1
 
-call npm.cmd run build || exit /b 1
-node.exe dist\index.js remote %*
+> "%LOG%" echo [%date% %time%] Starting MainRig Desktop Commander fork
+call npm.cmd run build >> "%LOG%" 2>&1 || exit /b 1
+node.exe dist\index.js remote %* >> "%LOG%" 2>&1
